c3e262d437f4154a2bd5edade9bec7f9a6b7ee1e
Momo Kornher <mail@moritzkornher.de>
2023-03-18 19:59:25 +0000

    ci: setup release (#1)

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,27 +1,57 @@
-import { typescript } from 'projen';
+import { github, release, typescript } from 'projen';
 const project = new typescript.TypeScriptProject({
   projenrcTs: true,
+
+  // Project info
   name: '@mrgrain/jsii-extend-interface',
   description: 'Make working with jsii interfaces great again',
+  authorName: 'Momo Kornher',
+  authorEmail: 'mail@moritzkornher.de',
+  authorUrl: 'https://moritzkornher.de',
   sampleCode: false,
 
-  defaultReleaseBranch: 'main',
-  release: false,
+  // TypeScript
+  tsconfig: {
+    compilerOptions: {
+      lib: ['es2022'],
+    },
+  },
 
-  deps: ['@jsii/spec'],
-  devDeps: ['projen'],
-  peerDeps: ['projen'],
-
+  // Formatting
   prettier: true,
   prettierOptions: {
     settings: {
       singleQuote: true,
     },
   },
-  tsconfig: {
-    compilerOptions: {
-      lib: ['es2022'],
+
+  // Automation
+  githubOptions: {
+    projenCredentials: github.GithubCredentials.fromApp(),
+    pullRequestLintOptions: {
+      semanticTitleOptions: {
+        types: ['feat', 'fix', 'chore', 'docs', 'ci', 'revert'],
+      },
     },
   },
+  autoApproveUpgrades: true,
+  autoApproveOptions: {
+    allowedUsernames: [
+      'projen-builder[bot]', // Bot account for upgrade PRs
+      'mrgrain', // Auto-approve PRs of main maintainer
+    ],
+  },
+
+  // Release
+  defaultReleaseBranch: 'main',
+  release: false,
+  releaseTrigger: release.ReleaseTrigger.scheduled({
+    schedule: '0 5 1 * *',
+  }),
+
+  // Dependencies
+  deps: ['@jsii/spec'],
+  devDeps: ['projen'],
+  peerDeps: ['projen'],
 });
 project.synth();
