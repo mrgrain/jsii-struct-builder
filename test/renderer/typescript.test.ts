65e2ea9cbdba4c3428d224f4cce871e6baa049c2
Momo Kornher <mail@moritzkornher.de>
2023-04-28 16:54:54 +0100

    one more test

--- test/renderer/typescript.test.ts
+++ test/renderer/typescript.test.ts
@@ -22,3 +22,26 @@ test('required properties do not render a default', () => {
   expect(renderedFile).not.toContain('@default');
   expect(renderedFile).not.toContain('foobar');
 });
+
+test('required properties can be forced to render a default', () => {
+  // ARRANGE
+  const renderer = new TypeScriptRenderer({
+    defaultTagsForRequiredProps: true,
+  });
+  const struct = Struct.empty('@my-scope/my-pkg.MyFunctionProps');
+  struct.add({
+    name: 'requiredProp',
+    type: { primitive: PrimitiveType.String },
+    optional: false,
+    docs: {
+      default: '"foobar"',
+    },
+  });
+
+  // ACT
+  const renderedFile = renderer.renderStruct(struct);
+
+  // ASSERT
+  expect(renderedFile).toMatchSnapshot();
+  expect(renderedFile).toContain('@default "foobar"');
+});
